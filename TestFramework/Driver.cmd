559ea9298d284609496b5685e373c52ff2be8405
rm <rich@moyse.us>
2019-07-05 20:51:33 -0700

    Tests run in nested cmd.exe call to allow Test Table's Command column to use environment variables

--- TestFramework/Driver.cmd
+++ TestFramework/Driver.cmd
@@ -10,19 +10,21 @@ goto Main
   echo ::-->&2
   echo ::--  Purpose:>&2
   echo ::--	Provide a testing framework to permit test driven development.>&2
-  echo ::--	A test is represented as a row with following columns:>&2
-  echo ::--		1. testName>&2
-  echo ::--		2. batchCommand - filepath name to batch command being tested followed by its arguments.>&2
-  echo ::--	A testName is separated from the filepath using '^|' (pipe) character.  Therefore a>&2
-  echo ::--	testName cannot contain this character.
+  echo ::--	A test is represented as a separate line (row) within a file.  This file is>&2
+  echo ::-- called a "Test Table".  Each row of this file must define the following>&2
+  echo ::-- columns:>&2
+  echo ::--		1. test description,>&2
+  echo ::--     2. errorlevel test,>&2
+  echo ::--		3. batch command.>&2
+  echo ::--	Columns are delinated by close bracket ('}').  Therefore column values>&2
+  echo ::--	cannot contain this character.>&2
   echo ::-- 
   echo ::-->&2
   echo ::--  Assumes:>&2
   echo ::-->&2
   echo ::--  Input:>&2
   echo ::--	1.  ^%1: Either:>&2
-  echo ::--		 	The full path name to a configuration file containing>&2
-  echo ::--		 	the test table.>&2
+  echo ::--		 	The full path name to a configuration file.>&2
   echo ::--			"/?" displays the "help".>&2
   echo ::-->&2
   echo ::--  Output:>&2
@@ -53,7 +55,7 @@ goto Main
   echo set TEST_TABLE_FILE="<TestTableAbsoluteFilePath>">&2
   echo ::>&2
   echo ::-- Direct test output to 'nul' device. Valid values: 'NO', 'SYSOUT', 'SYSERR', or 'BOTH'.  Default is 'BOTH'.>&2
-  echo ::-- Note: redirection operator ^> can be specified for each command in the test table.  Therefore, specifying 'NO'>&2
+  echo ::-- Note: redirection operator ^> can be specified for each command in the Test Table.  Therefore, specifying 'NO'>&2
   echo ::-- allows complete control of redirection by each command.>&2
   echo set TEST_NULLIFY_OUTPUT=BOTH>&2
   echo ::>&2
@@ -118,9 +120,16 @@ exit /b 0
   call :onFailureOpt %TEST_ON_FAILURE%
   if %errorlevel% NEQ 0 exit /b 1
 
+::-- Using environment variables below because variable assignment via set will preserve the
+::-- exact puncuation (especially quoting) specified for each column value.  At first, attempted
+::-- to pass each variable value by encapsulating in quotes, like "%%i", however, this
+::-- simple mechanism fails when the column values themselves contain quotes.
   set EXIT_LEVEL=0
   for /F "tokens=1,2,3* delims=}" %%i in ( 'type "%TEST_TABLE_FILE%"' ) do (
-    call :runCapture "%%i" "%%j" "%%k"  %SYSOUT% %SYSERR% || if "%TEST_ON_FAILURE%" == "EXIT" goto mainexit
+    set TEST_DESCRIPTION=%%i
+    set TEST_CONDITION=%%j
+    set TEST_COMMAND=%%k
+    call :runCapture %SYSOUT% %SYSERR% || if "%TEST_ON_FAILURE%" == "EXIT" goto mainexit
   )
 :mainexit:
 
@@ -175,11 +184,11 @@ exit /b 0
 ::-----------------------------------------------------------------------------
 :runCapture:
   setlocal
+
+  call :runTest %~1 %~2
   
-  call :runTest %3 %~4 %~5
-  
-  if %errorlevel% %~2 (
-    call :Abort "Test: '%~1' failed.  Command: '%~3'"
+  if %errorlevel% %TEST_CONDITION% (
+    call :Abort "Test: '%TEST_DESCRIPTION%' failed.  Command: '%TEST_COMMAND%'"
 	set EXIT_LEVEL=1
   )
 
@@ -188,17 +197,21 @@ exit/b %EXIT_LEVEL%
 
 ::-----------------------------------------------------------------------------
 ::- 
-::- Execute the test code after removing encapsulating parenthesis added by 
-::- caller.  This command can be complex.  For example the "command" can
-::- incorporate piping ('|').  Note: must prefix command with "call" keyword for
-::- most commands as they aren't built-in to the command processor, like echo,
-::- and will cause error.  Since even built-in commands can be prefixed with "call" 
-::- specifying "call" below improves resilency. 
+::- Execute the test code by starting another instance of the command processor.
+::- Calling another command processor evaluates the string which replaces 
+::- the environment variable TEST_COMMAND below allowing this string to use
+::- environment variables too.  Therefore, when coding the batch command 
+::- column most batch expressions are natively available - no special puncuation
+::- needed.  For example the TEST_COMMAND can incorporate piping ('|').
+::- 
+::- Note: use "call" keyword to invoke cmd.exe as this keyword will always 
+::- sucessfully execute cmd.exe even if future versions of windows batch releases 
+::- deviate from treating cmd as a built-in command.
 ::-  
 ::-----------------------------------------------------------------------------
 :runTest: 
 
-call %~1
+call cmd /c "%TEST_COMMAND%"
 
 exit /b
 
