bec1db6b6eb8a26d11eb65ae61013ce1c36dd311
rm <rich@moyse.us>
2019-07-24 15:52:41 -0400

    refactor diskpart to now call diskpartexecutor.cmd module, standardarize if statement format, include check to ensure some value was specified for GUID_BIND before calling function.

--- Vdisk/DerivedMerge.cmd
+++ Vdisk/DerivedMerge.cmd
@@ -8,26 +8,24 @@ goto Main
   echo ::--  Version:	1.0>&2
   echo ::--  Author:	Richard Moyse>&2
   echo ::-->&2
-  echo ::--  Purpose:>&2
-  echo ::--	Given a child/derived differencing VHD, merge its contents with its>&2
-  echo ::--	immediate parent VHD. Once merged, parent will be a mirror image>&2
-  echo ::--	of the child.>&2
+  echo ::-- Purpose:>&2
+  echo ::--	  Given a child/derived differencing VHD, merge its contents with its>&2
+  echo ::--	  immediate parent VHD. Once merged, parent will be a mirror image>&2
+  echo ::--	  of the child.>&2
   echo ::-->&2
-  echo ::--  Assumes:>&2
-  echo ::--	1.  Executing script with Administrator privileges.>&2
-  echo ::--	2.  Child VHD has been at least formatted and recognized as a Volume.>&2
+  echo ::-- Assumes:>&2
+  echo ::--   1. Executing script with Administrator privileges.>&2
+  echo ::-- 	2. Child VHD has been at least formatted and recognized as a Volume.>&2
   echo ::-->&2
-  echo ::--  Input:>&2
-  echo ::--	1.  ^%1: Either:>&2
-  echo ::--		 	The full path name to a configuration file containing>&2
-  echo ::--		 	argument values.>&2
+  echo ::-- Input:>&2
+  echo ::--   1. ^%1: Either:>&2
+  echo ::--		 	The full path name to a configuration file containing argument values.>&2
   echo ::--			"/?" displays the "help".>&2
   echo ::-->&2
-  echo ::--  Output:>&2
-  echo ::--	1.  errorlevel:>&2
-  echo ::--		0: Either:>&2
-  echo ::--			Successful execution of "/?">&2
-  echo ::--			Merge was successful>&2
+  echo ::-- Output:>&2
+  echo ::-- 1. errorlevel:>&2
+  echo ::--   0: Either:>&2
+  echo ::--     Successful execution of "/?">&2
   echo ::--		1: Failure>&2
   echo ::-->&2
   echo ::----------------------------------------------------------------------------->&2
@@ -49,8 +47,12 @@ goto Main
   echo ::>&2
   echo ::-- The absolute path, enclosed in double quotes, to the differencing (a.k.a. - child/derived) VHD >&2
   echo ::-- whose contents will be merged into its immediate parent VHD.>&2
-  echo set NATIVE_BOOT_DERIVED_FILE="<DerivedVHDAbsoluteFilePath>">&2
+  echo set DERIVED_LAYER_FILE="<DerivedVHDAbsoluteFilePath>">&2
   echo ::>&2
+  echo ::-- The absolute path, enclosed in double quotes, to the configuration file needed by the>&2
+  echo ::-- dispart executor.>&2
+  echo set DISKPART_EXECUTOR_CONFIG_FILE="<DiskpartExecutorAbsoluteFilePath>">&2
+  echo ::>&2 
   echo ::-- The absolute path, absent double quotes, to the directory that contains the logging methods.>&2
   echo set LOGGER_BIND=^<LogMethodsAbsoluteFilePath^>>&2
   echo ::>&2
@@ -67,20 +69,26 @@ exit /b 0
 
 
 :Main:
-  setlocal
+setlocal
 
-  if "%~1"==""       call :Abort "Please specify configuration file as first and only parameter.  Example follows:" & call :Help & exit /b 1
-  if "%~1"=="/?"     call :Help & exit /b 0
-  if not exist "%~1" call :Abort "Unable to locate provided configuration file:'%~1'.  Example follows:" & call :Help & exit /b 1
-  
-  call "%~1"
-  if errorlevel 1 call :Abort "Problem detected while processing paramters from configuration file '%~1'" & exit /b 1
-
-  call "%BIND_ARGUMENT%\Check" ARGUMENT_CHECK_EMPTY NATIVE_BOOT_DERIVED_FILE 
+  if "%~1"=="" (
+    call :Abort "Please specify configuration file as first and only parameter.  Example follows:"
+    call :Help
+    exit /b 1
+  )
+  if "%~1"=="/?" (
+    call :Help
+    exit /b 0
+  )
+  if not exist "%~1" (
+    call :Abort "Unable to locate provided configuration file:'%~1'.  Example follows:"
+    call :Help
+    exit /b 1
+  )
+   call "%~1"
   if errorlevel 1 (
-     call :Abort "Following configuration variables must be defined:'%ARGUMENT_CHECK_EMPTY%'"
-     call :Abort "Please correct errors in configuration file '%~1'"
-     exit /b 1
+    call :Abort "Problem detected while processing paramters from configuration file '%~1'"
+    exit /b 1
   )
   ::-- Determine if the transaction identifier has been defined before the configuration of this module.
   ::-- If it has, this module is a more primative element of an aggregate transaction.  Therefore, its
@@ -89,31 +97,34 @@ exit /b 0
   ::-- execution with the shared transaction identifier.  Otherwise, this module is being executed
   ::-- as a stand alone transaction, therefore, generate its own unique transaction id.
   if "%NHN.TRANSACTION_ID%"=="" (
-     call "%GUID_BIND%\gen" NHN.TRANSACTION_ID
-     if errorlevel 1 call :Abort "Generation of unique Transaction Id failed" & exit /b 1
+    if not "%GUID_BIND%" == "" (
+      call "%GUID_BIND%\gen" NHN.TRANSACTION_ID
+      if errorlevel 1 call :Abort "Generation of unique Transaction Id failed" & exit /b 1
+    )
+  )
+  call "%BIND_ARGUMENT%\Check" ARGUMENT_CHECK_EMPTY DERIVED_LAYER_FILE DISKPART_EXECUTOR_CONFIG_FILE
+  if errorlevel 1 (
+      if not exist "%BIND_ARGUMENT%\Check.cmd" (
+      call :Abort "Failed to bind argument check.  No Check method at filepath:'%BIND_ARGUMENT%\Check'"
+      exit /b 1
+    )
+     call :Abort "Following configuration variables must be defined:'%ARGUMENT_CHECK_EMPTY%'"
+     call :Abort "Please correct errors in configuration file '%~1'"
+     exit /b 1
   )
   ::-- Module is configured, now log the start of this effort.
-  call :Inform "Started: Derived VHD:'" %NATIVE_BOOT_DERIVED_FILE% "' merge to parent"
+  call :Inform "Started: Derived VHD:'" %DERIVED_LAYER_FILE% "' merge to parent"
+  
+  if not exist %DERIVED_LAYER_FILE% (
+    call :Abort "DERIVED_LAYER_FILE must exist to create new derived/child layer:'" %DERIVED_LAYER_FILE% "' does not exist or inaccessible due to permissions."
+    exit /b 1
+  )
+  call %~dp0\DiskpartExecutor.cmd %DISKPART_EXECUTOR_CONFIG_FILE%
+  if %errorlevel% neq 0 exit /b 1
+  
+  call :Inform "Ended: Derived VHD:'" %DERIVED_LAYER_FILE% "' merge to parent: Successful"
 
-  ::-- Create dispart merge command file 
-  set DISKPART_MERGE_CMD_FILE="%TEMP%\%~n0Script.%RANDOM%.txt"
-  echo select vdisk file=%NATIVE_BOOT_DERIVED_FILE% > %DISKPART_MERGE_CMD_FILE%
-  ::-- Can't merge if attached so first detach.  If not attached, detach will fail successfully permitting script to continue
-  echo detach vdisk NOERR >> %DISKPART_MERGE_CMD_FILE%
-  ::-- Merge current child with its immediate parent  
-  echo merge vdisk depth=1 >> %DISKPART_MERGE_CMD_FILE%
-  ::-- Merge current child with its immediate parent  
-  if not exist %DISKPART_MERGE_CMD_FILE% call :Abort "Could not create required Diskpart Command file named:" %NATIVE_BOOT_DERIVED_FILE% & exit /b 1
-  ::-- call diskpart to exexute the merge.
-  diskpart /s %DISKPART_MERGE_CMD_FILE% >nul
-  ::-- Determine if merge failed
-  if not errorlevel 0 call :Abort "Diskpart failed to merge:'" %NATIVE_BOOT_DERIVED_FILE% "'" & exit /b 1
-
-  ::-- remove temporary file
-  del %DISKPART_MERGE_CMD_FILE% >nul
-
-  ::-- Merge success!
-  call :Inform "Ended: Derived VHD:'" %NATIVE_BOOT_DERIVED_FILE% "' merge to parent: Successful"
+endlocal
 exit /b 0
 
 
