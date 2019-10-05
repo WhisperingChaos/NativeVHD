e478f5bc945784c1dc511ba9bda0124ac4fb22f0
rm <rich@moyse.us>
2019-10-04 21:49:18 -0400

    create common cleanup routine

--- Vdisk/Delete/Test/T006/run.cmd
+++ Vdisk/Delete/Test/T006/run.cmd
@@ -5,21 +5,35 @@ setlocal
     set VDISK_METHOD_PATH=..\..\..\..\Vdisk\
   )
   set DELETE_VDISK_FILE="%~dp0T006.VHD"
+
+  call :cleanup
+  if %errorlevel% NEQ 0 exit /b %errorlevel%
+  
   call "%VDISK_METHOD_PATH%\BaseCreate.cmd" "%~dp0\configCreate.cmd"
   if %errorlevel% NEQ 0 exit /b %errorlevel%
 
   call "%VDISK_METHOD_PATH%\Protect.cmd" "%~dp0\configProtect.cmd"
   if %errorlevel% NEQ 0 exit /b %errorlevel%
-
-  call "%VDISK_METHOD_PATH%\Delete.cmd" "%~dp0\config.cmd"
-  if %errorlevel% NEQ 1 (
-    echo Abort Delete of %DELETE_VDISK_FILE% should have returned a code of '1' to indicate failure.>&2
+  
+  call "%VDISK_METHOD_PATH%\Delete.cmd" "%~dp0\config.cmd" 2>&1 | findstr /R /C:"Abort.*DELETE_VHD_FILE.* is delete protected .read only. use ProtectWithdrawal.cmd to make file deletion possible"
+  if %errorlevel% NEQ 0 (
+    echo Abort Delete of %DELETE_VDISK_FILE% should have generated delete protect message.>&2
     exit /b 1
   )
   if not exist %DELETE_VDISK_FILE% (
     echo Abort %DELETE_VDISK_FILE% should remain because delete failed due to read only file attribute.>&2
     exit /b 1
   )
+  call :cleanup
+
+endlocal
+exit /b %errorlevel%
+
+
+:cleanup:
+
+  if not exist %DELETE_VDISK_FILE% exit /b 0
+
   call "%VDISK_METHOD_PATH%\ProtectWithdraw.cmd" "%~dp0\configProtectWithdraw.cmd"
   if %errorlevel% NEQ 0 exit /b %errorlevel%
 
@@ -30,5 +44,4 @@ setlocal
     echo Abort %DELETE_VDISK_FILE% should have been deleted.>&2
     exit /b 1
   )
-endlocal
-exit /b %errorlevel%
+exit /b 0
