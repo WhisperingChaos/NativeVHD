24e7f978fda05bd9d280f14d767b14f800dfa43a
rm <rich@moyse.us>
2019-10-04 21:53:00 -0400

    delete obsolete files

--- Vdisk/Esetup.cmd
+++
@@ -1,2 +0,0 @@
-subst Q: /D
-subst Q: "C:\Users\zServicePC\Desktop\PC Configuration\System Utilities\Utilities\Vdisk"
--- Vdisk/test.cmd
+++
@@ -1,5 +0,0 @@
-setlocal
-  BaseImageUpdate.cmd "C:\Users\zServicePC\Desktop\PC Configuration\System Utilities\Utilities\Vdisk\configuration\BaseImageUpdate.cmd"
-  echo errorlevel %errorlevel%
-pause
-exit /b
