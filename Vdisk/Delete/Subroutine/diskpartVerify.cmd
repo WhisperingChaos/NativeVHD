8330d706f363f84de35eb83a745de630de1c59c2
rm <rich@moyse.us>
2019-10-04 21:41:35 -0400

    verify runs the actual del.  It has been changed to capture error messages issued by del should it fail

--- Vdisk/Delete/Subroutine/diskpartVerify.cmd
+++ Vdisk/Delete/Subroutine/diskpartVerify.cmd
@@ -1,10 +1,10 @@
 :subroutine:
+setlocal
 
-  del %DELETE_VHD_FILE% >nul
-  if exist %DELETE_VHD_FILE% (
-    call :Abort "Can't delete: '" %DELETE_VHD_FILE% "'."
-    exit /b 1
-  )
+  for /F "delims=" %%M in ('del %DELETE_VHD_FILE% ^>nul 2^>^&1') do call :Abort "Delete of DELETE_VHD_FILE '" %DELETE_VHD_FILE% "' failed because: " "%%M"  
+  if %errorlevel% neq 0 exit /b 1
+    
+endlocal
 exit /b 0
 
 
@@ -12,3 +12,5 @@ exit /b 0
   echo /t "Abort" /p "%~f0" /m "%~1%~2%~3%~4%~5%~6%~7%~8%~9" >&2
   if not "%LOGGER_BIND%"=="" call "%LOGGER_BIND%\Record" %LOGGER_CONFIG_FILE% "%NHN.TRANSACTION_ID%" "Abort" "%~f0" "%~1%~2%~3%~4%~5%~6%~7%~8%~9"
 exit /b 1 
+
+
