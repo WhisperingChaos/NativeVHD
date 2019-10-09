fc75145837014ae46d2753ce0f667b29f6d89b11
rm <rich@moyse.us>
2019-10-09 15:40:06 -0400

    rename components labeled with DiskFormat to DriveFormat to correlate to object being affected by command

--- Vdisk/Delete/Test/T008/configDiskFormat.cmd
+++ Vdisk/Delete/Test/T008/configDiskFormat.cmd
@@ -2,19 +2,18 @@
 set BIND_ARGUMENT=%~dp0\..\..\..\..\Argument
 ::
 ::-- Required: The absolute path, enclosed in double quotes, to the VHD being formatted.
-set DISK_FORMAT_FILE=%TEST_VHD_FILE%
+set DISK_FORMAT_FILE=%DELETE_VDISK_FILE%
 ::
 ::-- The absolute path, enclosed in double quotes, to the configuration file needed by the
 ::-- dispart executor.
 set DISKPART_EXECUTOR_CONFIG_FILE="%~f0"
-::
 ::-- The absolute path, without double quotes, to a command that generates a
 ::-- cohesive set of diskpart commands.  Generator takes no arguments and produces
 ::-- commands as strings to SYSOUT.
-set DISKPART_CMD_GENERATOR=%~dp0\..\..\..\..\Vdisk\DriveFormat\Subroutine\diskpartCmdGen.cmd
+set DISKPART_CMD_GENERATOR=%~dp0\..\..\..\DiskFormat\Subroutine\diskpartCmdGen.cmd
 ::
 ::-- The absolute path, without double quotes, to a command that verifies diskpart's
 ::-- expected outcome.  Checker accepts no arguments - caller must use no overlapping
 ::-- environment variables that it sets before calling %~f0 to implement the Checker.
 ::-- A return value by the checker other than 0 signifies an error.
-set DISKPART_CONSTRAINT_CHECK=%~dp0\..\..\..\..\Vdisk\DriveFormat\Subroutine\diskpartVerify.cmd
+set DISKPART_CONSTRAINT_CHECK=%~dp0\..\..\..\DiskFormat\Subroutine\diskpartVerify.cmd
