5903fd330399794262fcac0a0ad9531e56001ed0
rm <rich@moyse.us>
2019-11-11 15:24:31 -0500

    image calls build on certain vdisk calls using a given config file.  binding for these calls and the config values should not be altered through the public config file interface :: remove the ability to dynamically change them.

--- Image/Revert/Test/T005/config.cmd
+++ Image/Revert/Test/T005/config.cmd
@@ -4,12 +4,6 @@ set BIND_ARGUMENT=%~dp0..\..\..\..\Argument\
 ::-- Required: The absolute path, without double quotes, to the Vdisk methods.
 set BIND_VDISK=%~dp0..\..\..\..\Vdisk\
 ::
-::-- Required: The absolute path, enclosed in double quotes, to the Vdisk config file needed by Delete.cmd.
-set REVERT_VDISK_DELETE_CONFIG="%~dp0"
-::
-::-- Required: The absolute path, enclosed in double quotes, to the config file required by LayerCreate.cmd.
-set REVERT_VDISK_LAYERCREATE_CONFIG="%~dp0"
-::
 ::-- Required: The absolute path, enclosed in double quotes, to the VHD layer being deleted.
 set REVERT_LAYER_FILE="%~dp0\ShouldNotExistLayer.vhd"
 ::
