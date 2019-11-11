3c73d9dafaec2173a3be248be2a8987a564e564f
rm <rich@moyse.us>
2019-11-11 15:17:52 -0500

    configuration settings for subroutines should use production time bind varialbe instead of test time variable

--- Image/Revert/Subroutine/configDelete.cmd
+++ Image/Revert/Subroutine/configDelete.cmd
@@ -7,11 +7,11 @@ set DISKPART_EXECUTOR_CONFIG_FILE="%~f0"
 ::-- The absolute path, without double quotes, to a command that generates a
 ::-- cohesive set of diskpart commands.  Generator takes no arguments and produces
 ::-- commands as strings to SYSOUT.
-set DISKPART_CMD_GENERATOR=%VDISK_METHODS%\Delete\Subroutine\diskpartCmdGen.cmd
+set DISKPART_CMD_GENERATOR=%BIND_VDISK%\Delete\Subroutine\diskpartCmdGen.cmd
 ::
 ::-- The absolute path, without double quotes, to a command that verifies diskpart's
 ::-- expected outcome.  Checker accepts no arguments - caller must use no overlapping
 ::-- environment variables that it sets before calling %~f0 to implement the Checker.
 ::-- A return value by the checker other than 0 signifies an error.
-set DISKPART_CONSTRAINT_CHECK=%VDISK_METHODS%\Delete\Subroutine\diskpartverify.cmd
+set DISKPART_CONSTRAINT_CHECK=%BIND_VDISK%\Delete\Subroutine\diskpartverify.cmd
 ::
--- Image/Revert/Subroutine/configLayerCreate.cmd
+++ Image/Revert/Subroutine/configLayerCreate.cmd
@@ -10,10 +10,10 @@ set DISKPART_EXECUTOR_CONFIG_FILE="%~f0"
 ::-- The absolute path, without double quotes, to a command that generates a
 ::-- cohesive set of diskpart commands.  Generator takes no arguments and produces
 ::-- commands as strings to SYSOUT.
-set DISKPART_CMD_GENERATOR=%VDISK_METHOD_BIND%\LayerCreate\subroutine\diskpartCmdGen.cmd
+set DISKPART_CMD_GENERATOR=%BIND_VDISK%\LayerCreate\subroutine\diskpartCmdGen.cmd
 ::
 ::-- The absolute path, without double quotes, to a command that verifies diskpart's
 ::-- expected outcome.  Checker accepts no arguments - caller must use no overlapping
 ::-- environment variables that it sets before calling %~f0 to implement the Checker.
 ::-- A return value by the checker other than 0 signifies an error.
-set DISKPART_CONSTRAINT_CHECK=%VDISK_METHOD_BIND%\LayerCreate\subroutine\diskpartVerify.cmd
+set DISKPART_CONSTRAINT_CHECK=%BIND_VDISK%\LayerCreate\subroutine\diskpartVerify.cmd
